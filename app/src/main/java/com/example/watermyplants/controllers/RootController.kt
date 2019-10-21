57d53791601a3b128281ae8d5a0112c3e8d41eb3
msmaitran <itsmaisn@gmail.com>
2019-10-21 12:46:26 -0700

    Create RegisterController and inflate registration_layout when "Register" button is clicked.

--- app/src/main/java/com/example/watermyplants/controllers/RootController.kt
+++ app/src/main/java/com/example/watermyplants/controllers/RootController.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.ControllerChangeHandler
+import com.bluelinelabs.conductor.ControllerChangeType
+import com.bluelinelabs.conductor.RouterTransaction
+import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
 import com.example.watermyplants.R
 
 class RootController: Controller {
@@ -12,9 +17,24 @@ class RootController: Controller {
     constructor(): super()
     constructor(args: Bundle?) : super(args)
 
+    // Inflate Login Layout
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.login_layout, container, false)
         return view
     }
 
+    override fun onChangeEnded(
+        changeHandler: ControllerChangeHandler,
+        changeType: ControllerChangeType
+    ) {
+        super.onChangeEnded(changeHandler, changeType)
+
+        view?.findViewById<Button>(R.id.btn_register)?.setOnClickListener {
+            router.pushController(
+                RouterTransaction.with(RegisterController(args))
+                    .pushChangeHandler(HorizontalChangeHandler())
+                    .popChangeHandler(HorizontalChangeHandler())
+            )
+        }
+    }
 }
