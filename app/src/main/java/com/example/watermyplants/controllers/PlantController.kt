53dc08e3194163df4880594e65bc7134a0890b81
msmaitran <itsmaisn@gmail.com>
2019-10-23 10:26:26 -0700

    Create NotificationGenerator.

--- app/src/main/java/com/example/watermyplants/controllers/PlantController.kt
+++ app/src/main/java/com/example/watermyplants/controllers/PlantController.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.lifecycle.Observer
-import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
 import com.example.watermyplants.App
